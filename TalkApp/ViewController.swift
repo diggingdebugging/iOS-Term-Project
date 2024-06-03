214990e4d9254322b0e76b2248ad2fb494577a5e
yujin <yujin3289@naver.com>
2024-06-03 13:43:47 +0900

    Initial Commit

---
+++ TalkApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkApp
+//
+//  Created by yujin on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
